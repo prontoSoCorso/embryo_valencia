e5e5d40beb5fcba9e5ed4f35a450ab5a65ddd7af
lorenzo_corso <lorenzo.corso01@universitadipavia.it>
2026-01-09 15:16:00 +0100

    final dataset

---
+++ _utils/remove_pswd.py
@@ -0,0 +1,29 @@
+import msoffcrypto
+
+# --- CONFIGURAZIONE ---
+input_path = "/home/phd2/Scrivania/CorsoRepo/embryo_valencia/dataset_final.xlsx"
+output_path = "/home/phd2/Scrivania/CorsoRepo/embryo_valencia/dataset.xlsx"
+
+# pswd
+excel_password = ""
+
+try:
+    # Apre il file criptato in modalità lettura binaria
+    encrypted_file = open(input_path, "rb")
+    file = msoffcrypto.OfficeFile(encrypted_file)
+
+    # Carica la chiave (password)
+    file.load_key(password=excel_password)
+
+    # Decripta e salva nel nuovo percorso in modalità scrittura binaria
+    with open(output_path, "wb") as f:
+        file.decrypt(f)
+
+    print("✅ Successo! Il file decriptato è stato salvato in:")
+    print(output_path)
+
+    # Chiude il file originale
+    encrypted_file.close()
+
+except Exception as e:
+    print(f"❌ Errore durante la decriptazione: {e}")
